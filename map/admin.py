95720dfd0a9d0c805a1b8a8bd981ac732dbfeb12
Rebrina <feurioster@gmail.com>
2019-10-31 22:28:05 +0300

    pr

---
+++ map/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from map.models import Route, Station
+
+admin.site.register(Route)
+admin.site.register(Station)
---
+++ map/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from map.models import Route, Station
+
+# Create your views here.
+
+def stations_views(request):
+	context = {}
+	context['routes'] = Route.objects.all()
+	if request == 'POST':
+		route = request.GET['route']
+		stations = Station.objects.filter(routes__name=route)
+		stations = stations.order_by('longitude')
+		st_first = stations.first()
+		st_last = stations.last()
+		x = (st_first.longitude + st_last.longitude) / 2
+		stations = stations.order_by('latitude')
+		st_first = stations.first()
+		st_last = stations.last()
+		y = (st_first.latitude + st_last.latitude ) / 2
+		context['stations'] = stations
+		context['center'] = {'y': y, 'x': x}
+	return render(request, 'stations.html', context)
